a07fcde25735ee5bdedf40c792b67dc685b0e555
latiagertrutis <teorodrip@posteo.net>
2025-04-02 21:23:25 +0200

    Add init ping

--- src/ping.c
+++ src/ping.c
@@ -1,6 +1,12 @@
+#include <asm-generic/socket.h>
 #include <stdio.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <errno.h>
+#include <string.h>
 
 #define HELP_STRING \
 "Usage: ft_ping [OPTION...] HOST ...\n" \
@@ -10,9 +16,53 @@
 "  -v                 verbose output\n" \
 "  -?                 give this help list\n"
 
+struct ping_s {
+    int fd;
+};
+
+typedef struct ping_s ping;
+
+static ping* ping_init() {
+    int fd;
+    struct protoent *proto;
+    ping *p = NULL;
+    int one = 1;
+
+    proto = getprotobyname("icmp");
+    if (proto == NULL) {
+        /* errno is not set by getprotobyname() */
+        errno = ENOPROTOOPT;
+        return NULL;
+    }
+
+    fd = socket(AF_INET, SOCK_DGRAM, proto->p_proto);
+    if (fd < 0) {
+        return NULL;
+    }
+
+    /* Ping may need to talk with a broadcast address */
+    if (setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &one, sizeof(one)) != 0) {
+        goto close_return;
+    }
+
+    p = malloc(sizeof(ping));
+    if (p == NULL) {
+        goto close_return;
+    }
+
+    p->fd = fd;
+
+    return p;
+
+close_return:
+    close(fd);
+    return NULL;
+}
+
 int main(int argc, char** argv) {
     int c;
     bool verbose = false;
+    ping *p;
 
     while ((c = getopt(argc, argv, "v?")) != -1) {
         switch (c) {
@@ -26,5 +76,13 @@ int main(int argc, char** argv) {
         }
     }
 
-    return 0;
+    p = ping_init();
+    if (p == NULL) {
+        perror("ping_init");
+        exit (EXIT_FAILURE);
+    }
+
+    close(p->fd);
+    free(p);
+    return EXIT_SUCCESS;
 }
