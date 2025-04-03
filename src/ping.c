f6a6cc5c80776d958ccd86f683eb376b0c57175f
latiagertrutis <teorodrip@posteo.net>
2025-04-03 18:47:45 +0200

    Add verbose printing

--- src/ping.c
+++ src/ping.c
@@ -1,4 +1,5 @@
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -19,18 +20,26 @@
 "  -?                 give this help list\n"
 
 #define ICMP_ECHO		8
+#define PING_HEADER_LEN 8
+#define PING_DATALEN	(64 - PING_HEADER_LEN)	/* default data length */
+
+/* Ping options */
+#define OPT_VERBOSE 0x01
 
 typedef struct ping_s {
-    int fd;
-    int type;
-    host *dest;
+    int				 fd;
+    int				 type;
+    unsigned short	 ident;
+    host			*dest;
+    size_t			 datalen;
+    char			 options;
 } ping;
 
-static ping* ping_init() {
-    int fd;
+static ping* ping_init(unsigned short ident) {
+    int				 fd;
     struct protoent *proto;
-    ping *p = NULL;
-    int one = 1;
+    ping			*p	 = NULL;
+    int				 one = 1;
 
     proto = getprotobyname("icmp");
     if (proto == NULL) {
@@ -53,8 +62,10 @@ static ping* ping_init() {
     if (p == NULL) {
         goto close_return;
     }
+    memset(p, 0, sizeof(ping));
 
     p->fd = fd;
+    p->ident = ident;
 
     return p;
 
@@ -67,13 +78,19 @@ static int ping_echo(ping * p, char *host) {
     int ret = 0;
 
     p->type = ICMP_ECHO;
+    p->datalen = PING_DATALEN;
     p->dest = ping_get_host(host);
     if (p->dest == NULL) {
         ret = -1;
         goto exit_clean;
     }
 
-    printf("ADDR NAME IS: %s\n", p->dest->name);
+    printf ("PING %s (%s): %zu data bytes", p->dest->name,
+            inet_ntoa(p->dest->addr.sin_addr), p->datalen);
+    if (p->options & OPT_VERBOSE) {
+        printf (", id 0x%04x = %u", p->ident, p->ident);
+    }
+    printf ("\n");
 
 exit_clean:
     free(p->dest);
@@ -82,7 +99,7 @@ exit_clean:
 
 int main(int argc, char** argv) {
     int c;
-    bool verbose = false;
+    bool verbose;
     ping *p;
 
     while ((c = getopt(argc, argv, "v?")) != -1) {
@@ -97,12 +114,17 @@ int main(int argc, char** argv) {
         }
     }
 
-    p = ping_init();
+    /* Initialize ping structure */
+    p = ping_init(getpid());
     if (p == NULL) {
         perror("ping_init");
         exit (EXIT_FAILURE);
     }
 
+    /* Write the options into the ping structure */
+    p->options |= verbose ? OPT_VERBOSE : 0x00;
+
+    /* Loop through all the hosts */
     for (; optind < argc; optind++) {
         if (ping_echo(p, argv[optind]) != 0) {
             perror("ping_echo");
